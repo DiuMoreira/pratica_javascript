781ea1366e6c72edb7be51e96c209a4f49b8ded5
DiuMoreira <diulypma@gmail.com>
2024-07-17 20:58:36 -0300

    Adicionando arquivo

---
+++ Pratica_1.js
@@ -0,0 +1,29 @@
+// Pratica 1
+
+let num1 = 55; 
+let num2 = 42;
+
+let soma = num1 + num2;
+
+console.log("A soma dos números é:", soma);
+
+
+// Pratica 2
+
+let nome = "Jason";
+let idade = 20; 
+
+let idadeFutura = idade + 10;
+
+console.log("Daqui a 10 anos, " + nome + " terá " + idadeFutura + " anos.");
+
+
+//Pratica 3
+let raio = 5;
+let valor_pi = 3.14;
+
+let area = valor_pi * raio * raio;
+let perimetro = 2 * valor_pi * raio;
+
+console.log(`A área do círculo é: ${area.toFixed(2)}!`);
+console.log(`A perímetro do círculo é: ${perimetro.toFixed(2)}!`);
